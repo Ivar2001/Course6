37113baeeabc5a1a5b08a3a300af770ffe173afc
Ivar2001 <iaoqi@student.han.nl>
2020-11-24 16:46:07 +0100

    Alle bestanden van onderwijs online

---
+++ edgeR.R
@@ -0,0 +1,98 @@
+### HAN (c) Todt
+library(limma)
+library(edgeR)
+
+###############################################################
+### read counts
+###############################################################
+
+fDir <-  "/home/ivar/Desktop/Han/Jaar2/Blok 2/project/"
+fName <- "WCFS1_cnts.txt"
+
+cnts <- read.delim(paste0(fDir,fName), comment.char="#")
+
+### used for topTags identification
+row.names(cnts) <- cnts[,"ID"]
+
+###############################################################
+### Create DGEList
+###############################################################
+
+exp <- c("WCFS1.glc","WCFS1.glc","WCFS1.rib","WCFS1.rib")
+
+group <- factor(exp)
+y <- DGEList(counts=cnts[,2:5],group=group)
+
+###############################################################
+### Normalise counts
+### Trimmed mean of M values : remove lowest and highest values
+### (percentile) and calculate mean
+### 
+###############################################################
+
+y <- calcNormFactors(y, method="TMM" )
+
+###############################################################
+### Check statistics
+###############################################################
+
+print("Count statistics")
+print(summary(y$counts))
+print(y$samples)
+
+###############################################################
+### Create design matrix
+###############################################################
+
+design <- model.matrix(~0+group, data=y$samples)
+colnames(design) <- levels(y$samples$group)
+print(design)
+
+###############################################################
+### Estimate Dispersion
+###############################################################
+
+#y <- estimateDisp(y, design)
+
+y <- estimateGLMCommonDisp(y,design)
+y <- estimateGLMTrendedDisp(y,design, method="power")
+y <- estimateGLMTagwiseDisp(y,design)
+
+###############################################################
+### Plot results
+###############################################################
+
+pdf(paste0(fDir,"LP_edgeR.pdf"))
+plotMDS(y)
+plotBCV(y)
+dev.off()
+
+###############################################################
+### Fit data
+###############################################################
+
+fit <- glmFit(y,design)
+
+###############################################################
+### Determine fold changes
+###############################################################
+
+mc <- makeContrasts(exp.r=WCFS1.glc-WCFS1.rib, levels=design)
+
+fit <- glmLRT(fit, contrast=mc)
+
+###############################################################
+### Print top tags
+###############################################################
+
+res<-topTags(fit)
+print(res)
+
+
+###############
+### t() = Transpose draaid de rij en colom om
+### kmeans(data) = clusteren van data
+### normalizeren
+### lage eruit filteren
+### p-value etc berekenen
+###############
