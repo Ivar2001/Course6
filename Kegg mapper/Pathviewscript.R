65ee83318eec8651908aded3b5cc4ed42545d87d
CommanderKronos <yuriwit0@gmail.com>
2021-01-06 12:32:51 +0100

    fixed pathviewscript.r finally

--- Kegg mapper/Pathviewscript.R
+++ Kegg mapper/Pathviewscript.R
@@ -9,22 +9,28 @@ neededWD <- paste(currWD, "/Kegg mapper", sep = "")
 setwd(neededWD)
 
 #Define pathway to be used (found on KEGG db)
-mypathway <- "lpl00010"
+glycolysispathway <- "lpl00010"
+pentosephyosphatepathway <- "lpl00030"
 
 #Load selected genes
-WCFS1genes <- read.xlsx("Selected genes.xls", 1)
-NC8genes <- read.xlsx("Selected genes.xls", 2)
+WCFS1genes <- read.xlsx("Merged_Genes.xlsx", 1)
+NC8genes <- read.xlsx("Merged_Genes.xlsx", 2)
 
 #Seperate logFC of selected genes
 WCFS1logFC <- WCFS1genes$logFC
 NC8logFC <- NC8genes$logFC
 
 #Set names of the data
-names(WCFS1logFC) <- row.names(WCFS1genes)
-names(NC8logFC) <- row.names(NC8genes)
+names(WCFS1logFC) <- WCFS1genes$NA.
+names(NC8logFC) <- NC8genes$NA.
 
 #Generate pathviews
-WCFS1.out <- pathview(gene.data = WCFS1logFC, species = "lpl", pathway = mypathway, gene.idtype = "KEGG")
+WCFS1.out <- pathview(gene.data = WCFS1logFC, species = "lpl", pathway = glycolysispathway, gene.idtype = "KEGG")
 file.copy("lpl00010.pathview.png", "WCFS1.lpl00010.pathview.png")
-NC8.out <- pathview(gene.data = NC8logFC, species = "lpl", pathway = mypathway, gene.idtype = "KEGG")
+NC8.out <- pathview(gene.data = NC8logFC, species = "lpl", pathway = glycolysispathway, gene.idtype = "KEGG")
 file.copy("lpl00010.pathview.png", "NC8.lpl00010.pathview.png")
+
+WCFS1.out <- pathview(gene.data = WCFS1logFC, species = "lpl", pathway = pentosephyosphatepathway, gene.idtype = "KEGG")
+file.copy("lpl00030.pathview.png", "WCFS1.lpl00030.pathview.png")
+NC8.out <- pathview(gene.data = NC8logFC, species = "lpl", pathway = pentosephyosphatepathway, gene.idtype = "KEGG")
+file.copy("lpl00030.pathview.png", "NC8.lpl00030.pathview.png")
